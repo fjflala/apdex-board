98fcc82f177fca33ec0d53d3f56506d60a838cf1
Francisco Felix <francisco.felix@ext.privalia.com>
2019-01-08 20:37:44 +0100

    Basic webpack config

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+/**
+ * Module dependencies
+ */
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+/**
+ * Webpack Copy plugin config
+ */
+const copyPlugin = new CopyWebpackPlugin([
+  {
+    from: './src/view/index.html',
+    to: './index.html',
+    force: true,
+    flatten: true,
+  }
+]);
+
+/**
+ * Webpack Clean plugin config
+ */
+const pathsToClean = [
+  './dist',
+];
+const cleanPlugin = new CleanWebpackPlugin(pathsToClean);
+
+/**
+ * Export
+ */
+module.exports = {
+  mode: process.env.NODE_ENV,
+  resolve: {
+    extensions: ['.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      }
+    ],
+  },
+  plugins: [
+    copyPlugin,
+    cleanPlugin,
+  ],
+};
