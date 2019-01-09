faad3950ffe99ce47c75e6fd5cabe1ccb48837d7
Francisco Felix <francisco.felix@ext.privalia.com>
2019-01-09 01:45:29 +0100

    mapAppsByHost helper

---
+++ src/utils/mapAppsByHost/index.js
@@ -0,0 +1,32 @@
+/**
+ * 
+ * @param {array} apps - Array of objects
+ * @returns {object} - Map of hosts
+ */
+export default function mapAppsByHost(apps) {
+  const map = {};
+  for (let i = 0; i < apps.length; i++) {
+    const app = apps[i];
+    const { 
+      name,
+      contributors,
+      version,
+      apdex,
+      host,
+    } = app;
+
+    for (let h = 0; h < host.length; h++) {
+      const hostName = host[h];
+      const data = {
+        name,
+        contributors,
+        version,
+        apdex
+      };
+
+      map[hostName] = map[hostName] ? [...map[hostName], data] : [data];
+    }
+  }
+  
+  return map;
+};
