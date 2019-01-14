5f033cd33bf2343af37740b19bd7be1f68b5a1a5
Francisco Felix <francisco.felix@ext.privalia.com>
2019-01-14 15:29:38 +0100

    Change collectFrom option

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,8 @@ module.exports = {
   collectCoverageFrom: [
     "**/*.{js,jsx}",
     "!**/node_modules/**",
+    "!**/coverage/**",
+    "!**/dist/**",
   ],
   coverageDirectory: './coverage',
 };
