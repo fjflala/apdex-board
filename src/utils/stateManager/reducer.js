d2442c5fb4c3208032005b04355b0d6225711612
Francisco Felix <francisco.felix@ext.privalia.com>
2019-01-14 22:39:37 +0100

    Added events to stateManager

--- src/utils/stateManager/reducer.js
+++ src/utils/stateManager/reducer.js
@@ -1,13 +1,26 @@
-import { REQUEST_SUCCESS, REMOVE_APP_FROM_HOST } from './actions';
+import {
+  REQUEST_SUCCESS,
+  REMOVE_APP_FROM_HOST,
+  ADD_APP_TO_HOSTS,
+} from './actions';
 
 export default function reducer(state = {}, eventName, payload) {
   switch(eventName) {
     case REQUEST_SUCCESS:
       return payload;
     case REMOVE_APP_FROM_HOST:
-      state.data[payload.hostName] = state.data[payload.hostName].filter(app => {
-        return app.name !== payload.name;
+      payload.hosts.forEach(hostName => {
+        state.data[hostName] = state.data[hostName].filter(app => {
+          return app.name !== payload.name;
+        });
       });
+      return state;
+    case ADD_APP_TO_HOSTS:
+      payload.hosts.forEach((hostName) => {        
+        state.data[hostName].push(payload.app);
+        state.data[hostName] = state.data[hostName].sort((a, b) => b.apdex - a.apdex);
+      });
+
       return state;
     default:
       return state;
