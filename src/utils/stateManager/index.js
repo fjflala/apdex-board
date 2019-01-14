71286a1c6d33fc10c39401207dbd762bb35c810a
Francisco Felix <francisco.felix@ext.privalia.com>
2019-01-14 16:37:47 +0100

    Added documentation and tests

--- src/utils/stateManager/index.js
+++ src/utils/stateManager/index.js
@@ -1,11 +1,21 @@
+/**
+ * Module dependencies
+ */
 import reducer from './reducer';
 
+/**
+ * StateManager, a little implementation of redux
+ */
 export default class StateManager {
   constructor(initialState = {}) {
     this.listeners = [];
     this.state = initialState;
   }
-
+  /**
+   * dispatch method
+   * @param {string} eventName - Name of the event 
+   * @param {object} payload - Data of the event
+   */
   dispatch(eventName, payload) {
 		this.state = reducer(this.state, eventName, payload);
     for (let i in this.listeners) {
@@ -14,7 +24,11 @@ export default class StateManager {
       }
     }
   }
-
+  /**
+   * subscribe method
+   * @param {any} id 
+   * @param {function} callback 
+   */
   subscribe(id, callback) {
     if (!callback || !id) {
       throw new Error('Please specify the listener and the ID');
@@ -28,7 +42,11 @@ export default class StateManager {
       this.listeners.push({id, callback});
     }
   }
-
+  
+  /**
+   * getState method
+   * @returns {object} - the state
+   */
   getState() {
     return this.state;
   }
---
+++ src/utils/stateManager/index.spec.js
@@ -0,0 +1,91 @@
+/**
+ * Module dependencies
+ */
+const StateManager = require('./index').default;
+const {
+  REQUEST_SUCCESS,
+  REMOVE_APP_FROM_HOST,
+} = require('./actions');
+
+describe('StateManager util', () => {
+  it('Should create a new instance of StateManger', () => {
+    const stateManager = new StateManager();
+    expect(stateManager).toBeInstanceOf(StateManager);
+  });
+
+  it('Should return the state when calling getState method', () => {
+    const stateManager = new StateManager();
+    expect(stateManager).toBeInstanceOf(StateManager);
+    expect(stateManager.getState()).toEqual({});
+  });
+
+  it('Should push a new listener when subscribe method is used', () => {
+    const stateManager = new StateManager();
+    expect(stateManager).toBeInstanceOf(StateManager);
+    
+    stateManager.subscribe('test', (state) => {
+      return state;
+    });
+
+    expect(stateManager.listeners).toHaveLength(1);
+  });
+
+  it('Should throw Error when id/callback params are not sent', () => {
+    const stateManager = new StateManager();
+    expect(stateManager).toBeInstanceOf(StateManager);
+    
+    expect(() => {
+      stateManager.subscribe();
+    }).toThrowError();
+  });
+
+  it('Should throw Error if callback param in subscribe is not a function', () => {
+    const stateManager = new StateManager();
+    expect(stateManager).toBeInstanceOf(StateManager);
+    
+    expect(() => {
+      stateManager.subscribe('test', 'not_a_function');
+    }).toThrowError();
+  });
+
+  it('Should dispatch an event & subscriber should be called', () => {
+    const stateManager = new StateManager();
+    expect(stateManager).toBeInstanceOf(StateManager);
+    const callback = jest.fn();
+    stateManager.subscribe('test', callback);
+    
+    stateManager.dispatch(REQUEST_SUCCESS, { foo: 'bar' });
+    expect(callback).toHaveBeenCalled();
+  });
+
+  it('Should dispatch REMOVE_APP_FROM_HOST event & subscriber should be called', () => {
+    const stateManager = new StateManager({
+      data: {
+        bar: [
+          {
+            name: 'foo',
+          },
+        ],
+      },
+    });
+
+    expect(stateManager).toBeInstanceOf(StateManager);
+    const callback = jest.fn();
+    stateManager.subscribe('test', callback);
+    
+    stateManager.dispatch(REMOVE_APP_FROM_HOST, { hostName: 'bar', name: 'foo' });
+    expect(callback).toHaveBeenCalled();
+    expect(stateManager.getState().data.bar).toHaveLength(0);
+  });
+
+  it('Should dispatch default event & subscriber should be called', () => {
+    const stateManager = new StateManager();
+
+    expect(stateManager).toBeInstanceOf(StateManager);
+    const callback = jest.fn();
+    stateManager.subscribe('test', callback);
+    
+    stateManager.dispatch('testEvent', {});
+    expect(callback).toHaveBeenCalled();
+  });
+});
