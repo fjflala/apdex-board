238b5cd90176f04bb7636e075db82b4f646571dc
Francisco Felix <francisco.felix@ext.privalia.com>
2019-01-14 10:23:03 +0100

    Save main component and target on memory

--- src/utils/renderDom/index.js
+++ src/utils/renderDom/index.js
@@ -1,3 +1,33 @@
-export default function renderDom(component, target) {
-  target.innerHTML = component.render();
+/**
+ * Module dependencies
+ */
+import Component from "../component";
+
+/**
+ * Helpers
+ */
+let componentMemory;
+let targetMemory;
+
+/**
+ * renderDOM append the main component into the DOM
+ * @param {Component} component - Main component
+ * @param {HTMLElement} target - DOM target
+ */
+export default function renderDom(component = componentMemory, target = targetMemory) {
+  if (!component || !target) {
+    throw new Error('Should define component and target params');
+  }
+  
+  target.innerHTML = '';
+  target.appendChild(component.render());
+  
+  // Safe variables, will be used in Component
+  if (!componentMemory) {
+    componentMemory = component;
+  }
+
+  if (!targetMemory) {
+    targetMemory = target;
+  }
 }
